418be7b5bff3a9eca2aaad17bab7f26eb9833f43
visualstudio <visualstudio@230-PC18>
2019-04-11 16:32:39 -0400

    11

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
 
-namespace ConsoleApp1
+namespace Assignment_4
 {
     class Program
     {
+        ArrayList Beowulf;
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+            p.Run();
+        }
+
+        public void Run() { this.ReadTextFiles(); }
+
+        public void ReadTextFiles()
+        {
+
+
+            StreamReader reader = new StreamReader("U:\Users\731777\beowolf.txt");
+            string script = reader.ReadToEnd();
+
+
+            var text = script.Trim();
+            int Count = 0, index = 0;
+
+
+            while (index < text.Length)
+            {
+
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+
+
+                Count++;
+
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+            }
+
+            Console.WriteLine("Total Number of Words are " + Count);
+
+        }
+
+
+        
         }
     }
-}
